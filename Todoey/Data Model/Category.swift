30be2eb76bce04ee13af766ecbad62246f388889
yu_m <koituha23@gmail.com>
2022-11-11 11:06:22 +0900

    CoreData complete

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Yusuke Mori on 2022/11/11.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,10 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Yusuke Mori on 2022/11/11.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
